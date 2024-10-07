f4144a1b470479361ceb424db7b470020a68ae13
vagante007 <106813312+vagante007@users.noreply.github.com>
2024-10-07 14:02:51 +0000

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,13 +1,9 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizarInformacoesGlobais() {
-
         const res = await fetch(url)
-
         const dados = await res.json()
-
-        console.log(dados);
-
+        console.log(dados.tempo_medio);
     }
 
     visualizarInformacoesGlobais()
