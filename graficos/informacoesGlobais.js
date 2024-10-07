00f81a6ad27f7b610eda2f54f534abf2c2b53516
vagante007 <106813312+vagante007@users.noreply.github.com>
2024-10-07 14:14:12 +0000

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,7 +8,7 @@ async function vizualizarInformacoesGlobais() {
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     
-    const container = document.getElementById(‘graficos-container’)
+    const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
   }
   
